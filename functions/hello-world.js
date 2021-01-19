0d5aba62491ee51b7d53cff5059e8e0e7c780a8c
Brian Ball <ideabrian@gmail.com>
2021-01-19 13:18:31 -0800

    Changed hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,13 @@
+// exports.handler = async () => {
+//   return {
+//     statusCode: 200,
+//     body: 'Hello egghead!',
+//   };
+// };
+
 exports.handler = async () => {
   return {
     statusCode: 200,
-    body: 'Hello egghead!',
+    body: 'Hello Brian watching tutorials!',
   };
 };
